0683d5005887569c91c0ddc099725bc8d8c5e12d
Giussepi <giussepexy@gmail.com>
2013-04-26 17:56:15 -0500

    adding fabric file

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+""" fabric's file """
+
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.hosts = ['giuseppi@198.101.153.252']
+
+def test():
+    local("./manage.py test baseapp")
+
+def push():
+    local("git push")
+
+def prepare_deploy():
+    test()
+    push()
+
+def deploy():
+    code_dir = '~giuseppi/testproject'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            run("git clone git@github.com:giussepi/testproject.git {}".format(
+                code_dir))
+    with cd(code_dir):
+        run("git pull")
+    with cd(code_dir + 'testproject'):
+        with settings(warn_only=True):
+            if run("nohup ./manage.py runserver &").failed:
+                pass
