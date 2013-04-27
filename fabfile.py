a2605e370a49ccaf9f952e78659ac3a4228d678a
Giussepi <giussepexy@gmail.com>
2013-04-27 00:57:50 -0500

    jjj

--- fabfile.py
+++ fabfile.py
@@ -26,7 +26,7 @@ def deploy():
 
     with cd(code_dir):        
         run("git pull")
-        run("source ../env/bin/activate && pip install -r requirements.txt && ./manage.py collectstatic --noinput")
+        # run("source ../env/bin/activate && pip install -r requirements.txt && ./manage.py collectstatic --noinput")
 
         # the following line is not working :(
-        # run("source ../env/bin/activate && pip install -r requirements.txt && nohup python manage.py runserver 0.0.0.0:8000 >& /dev/null < /dev/null &")
+        run("source ../env/bin/activate && pip install -r requirements.txt && ./manage.py collectstatic --noinput && nohup ./manage.py runserver 0.0.0.0:8000 >& /dev/null < /dev/null &")
