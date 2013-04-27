59f64f67cf8f64c63ba13ff0135caad603c7c571
Giussepi <giussepexy@gmail.com>
2013-04-27 01:05:08 -0500

    dfdf

--- fabfile.py
+++ fabfile.py
@@ -25,13 +25,13 @@ def deploy():
             run("virtualenv --distribute ../env")
 
     with cd(code_dir):
+        run("git pull")        
         commands_tuple = (
             'source ../env/bin/activate',
             'pip install -r requirements.txt',
             './manage.py collectstatic --noinput',
         )
-        run("git pull")
-        run (" && ".join(commands_tuple))
+        run(" && ".join(commands_tuple))
 
         # the following line is not working :(
         # run("source ../env/bin/activate && pip install -r requirements.txt && ./manage.py collectstatic --noinput && nohup ./manage.py runserver 0.0.0.0:8000 >& /dev/null < /dev/null &")
